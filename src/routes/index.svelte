4d5297f3be7c9cafbf5a873363c1ad2b13fb7f0a
TheComputerM <mudit.harshit99@outlook.com>
2021-04-01 18:26:41 +0530

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script>
+	import MaterialApp from "svelte-materialify/src/components/MaterialApp/MaterialApp.svelte";
+	import Button from "svelte-materialify/src/components/Button/Button.svelte";
+</script>
+
+<MaterialApp>
+	<Button>Hello</Button>
+</MaterialApp>
